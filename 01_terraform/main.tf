ea7a68af7bc69c67a374bce456494f58666b26d3
Adrian Navarro <adrian.navarro@seqera.io>
2025-04-23 12:23:15 +0200

    Add skeleton for terraform

---
+++ 01_terraform/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "3.0.1-rc8" # or latest available
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url      = "https://pve.adrilab.com/api2/json"
+  pm_user         = "root@pam"
+  pm_password     = "Estartus1!"
+  pm_tls_insecure = true
+  
+}
+
+
+resource "proxmox_vm_qemu" "my_vm" {
+  name        = "my-new-vm"
+  target_node = "pve"
+  clone       = "ubuntu-template"
+
+  ipconfig0 = "ip=10.0.10.111/24,gw=10.0.10.1"
+
+  os_type = "cloud-init"
+  ciuser  = "ubuntu"
+  cipassword = "securepassword"
+
+  sshkeys = file("~/.ssh/id_rsa.pub")
+}

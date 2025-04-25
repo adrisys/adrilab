ab4e05e5150f415cd5285eca5fe0b92a6e00e0b6
Adrian Navarro <adrian.navarro@seqera.io>
2025-04-25 14:25:49 +0200

    deploys correctly

--- 01_terraform/main.tf
+++ 01_terraform/main.tf
@@ -15,34 +15,76 @@ provider "proxmox" {
   
 }
 
-resource "proxmox_vm_qemu" "cloud_vm" {
-  name        = "my-cloudinit-vm"
-  target_node = "pve"
-  clone       = "ubuntu-template"  # Template VM name or ID
-  full_clone  = true
+resource "proxmox_vm_qemu" "cloudinit-test" {
+    name = "terraform-test-vm"
+    desc = "A test for using terraform and cloudinit"
 
-  cores       = 2
-  memory      = 2048
+    # Node name has to be the same name as within the cluster
+    # this might not include the FQDN
+    target_node = "pve"
 
-  os_type     = "cloud-init"
+    # The destination resource pool for the new VM
+#    pool = "pool0"
 
-  disk {
-    slot     = "scsi0"
-    size     = "40G"
-    type     = "disk"
-    storage  = "local-lvm"
-    iothread = true
-  }
+    # The template name to clone this vm from
+    clone = "VM 9000"
 
-  network {
-    id      = 0
-    model   = "virtio"
-    bridge  = "vmbr0"
-  }
+    # Activate QEMU agent for this VM
+    agent = 0
 
-  # Cloud-init config
-  ipconfig0 = "ip=dhcp"
-  sshkeys   = file("~/.ssh/id_rsa.pub")
-  ciuser    = "adrian"
-  cicustom  = ""  # Optional, for custom cloud-init ISO
+    os_type = "cloud-init"
+    cores = 2
+    sockets = 1
+    vcpus = 0
+    memory = 2048
+    scsihw = "virtio-scsi-pci"
+
+    # Setup the disk
+    disks {
+        ide {
+            ide2 {
+                cloudinit {
+                    storage = "local-lvm"
+                }
+            }
+        }
+        scsi {
+            scsi0 {
+                disk {
+                    size            = 32
+                    cache           = "writeback"
+                    storage         = "local-lvm"
+                    #storage_type    = "rbd"
+                    #iothread        = true
+                    #discard         = true
+                    replicate       = true
+                }
+            }
+        }
+    }
+
+    # Setup the network interface and assign a vlan tag: 256
+    network {
+        id     = 0
+        model  = "virtio"
+        bridge = "vmbr0"
+        #tag = 256
+    }
+
+    # Setup the ip address using cloud-init.
+    boot = "order=scsi0"
+    # Keep in mind to use the CIDR notation for the ip.
+    #ipconfig0 = "ip=192.168.10.20/24,gw=192.168.10.1"
+    ipconfig0 = "ip=dhcp"
+    #sshkeys = <<EOF
+    #ssh-rsa 9182739187293817293817293871== user@pc
+    #EOF
+    serial {
+      id   = 0
+      type = "socket"
+    }
+    
+    ciuser = "adri"
+    cipassword = "yourpassword"
+
 }
